4fdc79915745940a4eef24971cb13c367b1902d1
ashikmahmud <ashikmamud187@gmail.com>
2022-10-26 16:31:39 +0600

    update prot

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 require("dotenv").config();
 const { initSSLCommerz } = require("./controllers/payment.controller");
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 //middle wares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const port  = process.env.PORT || 5000;
 //routes
 app.get("/", (req, res) => {
   res.send({
@@ -17,21 +20,27 @@ app.get("/", (req, res) => {
   });
 });
 
-
 // payment routes
-app.get("/api/payment/init", initSSLCommerz)
+app.get("/api/payment/init", initSSLCommerz);
 
 //payment success route
 app.post("/success", (req, res) => {
-    console.log(req.body?.bank_tran_id , req.body?.status, req?.body?.amount, req.body?.card_type);
+  console.log(
+    req.body?.bank_tran_id,
+    req.body?.status,
+    req?.body?.amount,
+    req.body?.card_type
+  );
 
-    res.redirect(`http://127.0.0.1:5500/index.html`)
-    
-})
-
-
+  res.redirect(`http://127.0.0.1:5500/index.html`);
+});
 
 //listen this app
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("server is running on port 5000");
 });
+
+console.log(
+  "%cStop!",
+  "color:red;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold"
+);
